c07de37c823d29e9e7434e21e16df83c28cd5d65
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 17:36:45 +1000

    Refactor password_check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -4,8 +4,21 @@ Password Check Program
 
 MINIMUM_LENGTH = 6
 
-password = input("Enter a password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"Error. Password not longer than {MINIMUM_LENGTH} characters.")
-    password = input("Enter a password: ")
-print("*"*len(password))
+
+def main():
+    password = get_password()
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Error. Password not longer than {MINIMUM_LENGTH} characters.")
+        password = get_password()
+    display_stars(password)
+
+
+def display_stars(password):
+    print("*" * len(password))
+
+
+def get_password():
+    return input("Enter a password: ")
+
+
+main()
