101032c87c3b9495d55ae4e36bd3515b954a037e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 18:12:01 +1000

    Refactor broken_score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,12 +3,25 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """Get user score and determine grade using it"""
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    grade = determine_grade(score)
+    print(f"Your grade is {grade}.")
+
+
+def determine_grade(score):
+    """Determine grade from a score"""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
