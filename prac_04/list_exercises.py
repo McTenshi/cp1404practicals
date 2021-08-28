a645e926f9a7ae808985444c3b3eb72e2cdde048
McTenshi <unconfigured@null.spigotmc.org>
2021-08-28 12:53:12 +1000

    Add list_comprehensions program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,18 +3,18 @@ List Exercises Programs
 
 """
 
-# # 1. Basic list operations
-# numbers = []
-# for i in range(0, 5):
-#     number = int(input("Number: "))
-#     numbers.append(number)
-# print(f"The first number is {numbers[0]}")
-# print(f"The last number is {numbers[-1]}")
-# numbers.sort()
-# print(f"The smallest number is {numbers[0]}")
-# print(f"The largest number is {numbers[-1]}")
-# average = sum(numbers) / len(numbers)
-# print(f"The average of the numbers is {average}")
+# 1. Basic list operations
+numbers = []
+for i in range(0, 5):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+numbers.sort()
+print(f"The smallest number is {numbers[0]}")
+print(f"The largest number is {numbers[-1]}")
+average = sum(numbers) / len(numbers)
+print(f"The average of the numbers is {average}")
 
 # 2. Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
