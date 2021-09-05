9908e58776c46ddd0e92b3a1b69555bea58df465
McTenshi <unconfigured@null.spigotmc.org>
2021-09-05 23:47:11 +1000

    Add get_name_from_email function Add confirmation code

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,7 +4,21 @@ emails program
 
 """
 
-email_to_name = {}
-email = input("Email: ")
-name = input("Name: ")
-email_to_name[email] = name
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/N) ").upper()
+        if confirmation != "Y":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+
+def get_name_from_email(email):
+    first_part = email.split("@")
+    name_parts = first_part.split(".")
+    name = " ".join(name_parts).title()
+    return name
