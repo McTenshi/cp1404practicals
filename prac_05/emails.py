9d91ca73013e8805c66ccaba774ca54bf513b0ec
McTenshi <unconfigured@null.spigotmc.org>
2021-09-05 23:58:03 +1000

    Amend confirmation parameters Add code to display dictionary Fix code for get_name_from_email function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,14 +11,18 @@ def main():
     while email != "":
         name = get_name_from_email(email)
         confirmation = input(f"Is your name {name}? (Y/N) ").upper()
-        if confirmation != "Y":
+        if confirmation != "Y" and confirmation != "":
             name = input("Name: ")
         email_to_name[email] = name
         email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} {email}")
 
 
 def get_name_from_email(email):
-    first_part = email.split("@")
+    first_part = email.split("@")[0]
     name_parts = first_part.split(".")
     name = " ".join(name_parts).title()
     return name
+
+main()
