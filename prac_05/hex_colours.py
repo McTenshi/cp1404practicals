60d0d4cfc30ec2b523a5f33dc41f9dc8afc506d5
McTenshi <unconfigured@null.spigotmc.org>
2021-09-05 16:00:30 +1000

    Add dictionary of hex colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,9 @@
+"""
+1402 - Practical Joshua Quidlat
+Hex Colours
+"""
+
+HEX_COLOURS = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "aquamarine" : "#7fffd4", "azure": "#f0ffff",
+               "beige": "#f5f5dc", "black": "#000000", "blue": "#0000ff", "brown": "#a52a2a", "cyan": "#00ffff",
+               "green": "#00ff00"}
+
