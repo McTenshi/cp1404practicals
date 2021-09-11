9ed55ba0ca32c4e35859b32689bab18c08359a82
McTenshi <unconfigured@null.spigotmc.org>
2021-09-11 14:39:21 +1000

    Initial commit

---
+++ prac_06/programming_language.py
@@ -0,0 +1,19 @@
+"""
+CP1404 - Practical
+Joshua Quidlat
+Programming Language
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
