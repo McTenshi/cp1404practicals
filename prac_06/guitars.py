b511f88e9c7fa3b341f5bc252786c62d39ede392
McTenshi <unconfigured@null.spigotmc.org>
2021-09-19 10:15:25 +1000

    Remove test code

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,21 +8,15 @@ from guitar import Guitar
 guitars = []
 
 print("My guitars!")
-# name = input("Name: ")
-# while name != "":
-#     year = int(input("Year: "))
-#     cost = float(input("Cost: $"))
-#     guitars.append(Guitar(name, year, cost))
-#     name = input("Name: ")
-
-# Test guitars to skip user input
-guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitars.append(Guitar(name, year, cost))
+    name = input("Name: ")
 
 print("These are my guitars:")
 for i, guitar in enumerate(guitars, 1):
     vintage_string = " (vintage)" if guitar.is_vintage() else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
-# print(guitars)
-# print(guitars[0])
